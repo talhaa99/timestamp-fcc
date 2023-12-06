9777848d53dcda25972a0a3c03a1fbdee9eab6a2
Talha Maqsood <68240954+talhaa99@users.noreply.github.com>
2023-12-06 11:07:19 +0500

    ### Task
    - Timestamp Microservice

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.get("/api/hello", function (req, res) {
     res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+    const date = new Date();
+    return res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
+
 app.get("/api/:date", function (req, res) {
 
     let reqDate = req.params.date;
