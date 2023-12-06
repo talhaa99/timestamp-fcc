6e4d370f204059f80b88406d9a5fa77c3beae28a
Talha Maqsood <68240954+talhaa99@users.noreply.github.com>
2023-12-06 10:54:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+    res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date", function (req, res) {
+
+    let reqDate = req.params.date;
+
+    if (!isNaN(reqDate)) {
+        reqDate = parseInt(reqDate);
+    }
+
+    const date = new Date(reqDate);
+
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
